ebafdffd6efe1c6d7d7c65dae4350eff8764f6de
Ziggy Chen <ziggycs9796@hotmail.com>
2024-04-17 18:10:48 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <Tiptap />
+</template>
+
+<script setup>
+import Tiptap from './components/Tiptap.vue'
+
+</script>
---
+++ src/components/Tiptap.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="w-full h-full flex">
+    <div class="w-40 h-full bg-gray-50 flex flex-col">
+      <el-button type="primary" class="mt-4 !mx-2" @click="getHTML">html</el-button>
+      <el-button type="primary" class="mt-4 !mx-2" @click="getJSON">json</el-button>
+      <el-button type="primary" class="mt-4 !mx-2" @click="setOptions">setOptions</el-button>
+      <el-button type="primary" class="mt-4 !mx-2" @click="toggleEditable">toggleEditable</el-button>
+
+    </div>
+    <editor-content :editor="editor" />
+  </div>
+</template>
+
+<script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+
+const editor = useEditor({
+  content: `<p class="w-10" style="background: white">I’m running Tiptap with Vue.js. 🎉</p>`,
+  extensions: [
+    StarterKit,
+    Document
+  ],
+})
+
+
+function toggleEditable() {
+  editor.value.setEditable(!editor.value.isEditable)
+}
+function setOptions() {
+  editor.value?.setOptions({
+    editorProps: {
+      attributes: {
+        class: 'w-30'
+      }
+    }
+  })
+}
+
+function getHTML() {
+  console.log(editor.value.getHTML())
+  console.log(editor.value.$doc)
+}
+
+
+function getJSON() {
+  console.log(editor.value.getJSON())
+}
+
+</script>
